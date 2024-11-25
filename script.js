cbcdcb5aebf6ec0ca79735443583aa40add62790
mahidine-z <Djaffar.zennaf@icloud.com>
2024-11-25 15:56:04 +0100

    papier caillou ciseaux

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-// role : permet de faire un choix //
-// parametre:non //
-// return : Le choix du joueur //
-function choixJoueur() {
-    return prompt("faites un choix")
-}
-
-function choixOrdi() {
-    return Math.ceil(Math.random() * 3)
-}
-
-function Comparer (nombre) {
-    let result;
-    if (nombre === 1) {
-        result = `papier`;
-    }
-
-    { else if (nombre === 2)
-        result = `caillou`
-    }
-
-    {else if (nombre === 3)
-        result = `feuille`;
-    }
-
-
-}
-
-function Afficher() {
-
-}
-
-function Jouer() {
-
-}
+//role : faire une fonction qui modifie mon h1 avec le exte qui sera passée en périmetre
+//parametre: non
+//retour: oui
