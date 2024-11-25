65df582a68f2ceabd87f233e0844042a3873c3c2
mahidine-z <Djaffar.zennaf@icloud.com>
2024-11-25 14:42:11 +0100

    papier caillou ciseaux

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-function choixJoueur{
-    ciseaux > feuille
-    feuille > pierre
-    pierre  > ciseaux
+// role : permet de faire un choix //
+// parametre:non //
+// return : Le choix du joueur //
+function choixJoueur() {
+    return prompt("faites un choix")
+}
+
+function choixOrdi() {
+    return Math.ceil(Math.random() * 3)
+}
+
+function Comparer (nombre) {
+    let result;
+    if (nombre === 1) {
+        result = `papier`;
+    }
+
+    { else if (nombre === 2)
+        result = `caillou`
+    }
+
+    {else if (nombre === 3)
+        result = `feuille`;
+    }
+
+
+}
+
+function Afficher() {
+
+}
+
+function Jouer() {
+
 }
