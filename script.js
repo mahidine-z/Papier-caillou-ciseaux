d1de9bdfbb84a15d6c0f0ac1cc5432ddf18b9a15
mahidine-z <Djaffar.zennaf@icloud.com>
2024-11-26 11:55:25 +0100

    p-c-c

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // parametre:non
 // retour : le choix du joueur
 
-function choixJoueur(){
-    let choixJ=prompt("Choisissez feuille, ciseaux ou pierre");
+function choixJoueur() {
+    let choixJ = prompt("Choisissez papier, ciseaux ou pierre");
     return choixJ;
 }
 
@@ -13,35 +13,69 @@ function choixJoueur(){
 //retour: le choix de l'ordinateur sous forme de chaine de caractère
 
 
-function choixOrdi(){
-    return Math.ceeil(Math.random()*3;)
+function choixOrdi() {
+    return Math.ceil(Math.random() * 3)
 }
+
+function conversion(nbr){
+    let choixO;
+    if(nbr===1){
+        choixO="papier"
+    }
+    else if (nbr===2){
+        choixO="ciseaux"
+    }
+else if(nbr===3)
+    choixO="pierre"
+return choixO;
+
+}
+
+
+
+
+
 
 //role: Comparer le choix du joueur et le choix de l'ordi
 //parametre: choix du joueur et choix de l'ordinateur
 //retour:Le gagnant
 
-function compare(choixJoueur,ChoixOridnateur)
+function compare(choixJ, choixOrdinateur) {
 
-let result;
-if(choixJoueur=== choixOrdinateur){
-    result="égalité"
+    let result;
+    if (choixJoueur === choixOrdinateur){
+        result = "égalité"
+        
+    }else if((choixJoueur === "ciseaux" && choixOrdinateur === "papier") ||
+    (choixJoueur === "papier" && choixOrdinateur === "pierre") ||
+    (choixJoueur === "pierre" && choixOrdinateur === "ciseaux")){
+        result = "Vous avez gagné"
+    }else{
+        result="l'ordi a gagné"
+    }
+
+    return result
+      
 }
 
-else if {
-   (choixJoueur==="ciseaux"&& choixOrdinateur==="papier")||
-   (choixJoueur==="papier"&& choixOrdinateur==="pierre")||
-   (choixJoueur==="pierre"&& choixOrdinateur==="ciseaux")||
-    else
-}
-{
-    result="Vous avez gagné"
-}
+
 
 // role: afficher les choix et le gagnant dans un alert
 // parametre: choix du joueur, choix de l'ordinateur, resultat
 // retour:
 
-function affiche(choixJ, choixO, resultat){
-    alert(`choix du joueur`:${choix} // choix de l'ordinateur: $ { choixO}//
+function affiche(choixJ, choixO, resultat) {
+    alert(`choix du joueur: ${choixJ} // choix de l'ordinateur: ${choixO}//${resultat}`)
+}
+
+
+//role:lancer le jeu en lancant les fonctionsdans le bon ordre
+// parametre:non
+// retour:non
+function jouer() {
+    let choixJ = choixJoueur();
+    let nombre = choixOrdi()
+    let choixO = conversion(nombre);
+    let result= compare(choixJ, choixO);
+    affiche(choixJ, choixO, result);
 }
